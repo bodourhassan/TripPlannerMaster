baf151a2bfe1b9cb3122a58f503511cee4904d90
heshamkadry <hmohamedabdelkader@fcih1.com>
2018-03-18 17:25:44 +0200

    initial commit

--- app/src/main/java/com/ititeam/tripplannermaster/activity/MainActivity.java
+++ app/src/main/java/com/ititeam/tripplannermaster/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ititeam.tripplannermaster;
+package com.ititeam.tripplannermaster.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ititeam.tripplannermaster.R;
 
 public class MainActivity extends AppCompatActivity {
 
